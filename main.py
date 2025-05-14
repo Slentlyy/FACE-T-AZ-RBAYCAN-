8efcee3e5cdc099145b59d07698ba67e157a6c02
Slentlyy <sez1nko16@gmail.com>
2025-05-14 09:32:56 +0400

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
+import os
 import discord
 from discord.ext import commands
-import asyncio
-import os
 
-from cogs.match_module import JoinView, PickPlayerView, MapBanView
+TOKEN = os.getenv("DISCORD_TOKEN")  # Railway-də əlavə edəcəyik
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} olaraq daxil oldum.")
+    print(f'{bot.user} ilə giriş edildi.')
 
-    # Persistent düymələr aktiv olsun
-    bot.add_view(JoinView())
-    bot.add_view(PickPlayerView())
-    bot.add_view(MapBanView())
-
-    try:
-        synced = await bot.tree.sync()
-        print(f"Slash komandaları sinxronlaşdırıldı ({len(synced)} komanda).")
-    except Exception as e:
-        print(f"Komandalar sinxronlaşdırılarkən xəta baş verdi: {e}")
-
-async def main():
-    async with bot:
-        await bot.load_extension("cogs.match_module")
-        await bot.start(os.environ["DISCORD_TOKEN"])  # Tokeni burada dəyişməyin!
-
-asyncio.run(main())
+bot.run(TOKEN)
